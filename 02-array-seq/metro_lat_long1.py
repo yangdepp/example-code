2842593e49b5d36ab16ab9a48a5a7d2d6e9f7842
yangdepp <965093975@qq.com>
2018-05-07 23:54:44 +0800

    具名元组

--- 02-array-seq/metro_lat_long1.py
+++ 02-array-seq/metro_lat_long1.py
@@ -29,3 +29,34 @@ print(tokyo)
 print(tokyo.population)
 print(tokyo.coordinates)
 print(tokyo[1])
+print('#'*50)
+
+#具名元组的属性和方法
+#_fields属性是一个包含这个类所有字段名称的元组
+print(City._fields)   #('name', 'country', 'population', 'coordinates')
+
+#用_make()通过接受一个可迭代对象来生成这个类的一个实例，它的作用跟City(*delhi_data)是一样的
+LatLong = namedtuple('LatLong', 'lat long')
+delhi_data = ('Delhi NCR', 'IN', 21.935, LatLong(28.613889, 77.208889))
+print(delhi_data)
+delhi = City._make(delhi_data)
+print(delhi)
+print(delhi._asdict())    
+#_asdict()把具名元组以collections.OrderedDict的形式返回。
+#OrderedDict([('name', 'Delhi NCR'), ('country', 'IN'), ('population', 21.935), ('coordinates', LatLong(lat=28.613889, long=77.208889))])
+
+for key, value in delhi._asdict().items():
+    print(key + ':', value)
+
+
+
+
+
+
+######
+#####
+#知识点总结：
+'''
+    1、元组拆包
+    2、具名元组
+'''
